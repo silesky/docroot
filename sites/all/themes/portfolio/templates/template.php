d470365da3f23462ebfd745b7faf36a5d36e16e1
Seth Silesky <seth.silesky@gmail.com>
2016-03-31 11:35:44 -0500

    revert 'add template.php'

--- sites/all/themes/portfolio/templates/template.php
+++
@@ -1,7 +0,0 @@
-<?php function portfolio_process_page(&$variables) {
-  // Add theme suggestion for all content types
-    if (!empty($variables['node']) && !empty($variables['node']->type)) {
-      $variables['theme_hook_suggestions'][] = 'page__node__' . $variables['node']->type;
-    }
-  }
-?>

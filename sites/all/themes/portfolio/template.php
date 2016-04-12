99b411bb3b492d5ab4ff29a6a20567bea1556e6b
Seth Silesky <seth.silesky@gmail.com>
2016-04-12 10:12:00 -0500

    appearance settings: misc

--- sites/all/themes/portfolio/template.php
+++ sites/all/themes/portfolio/template.php
@@ -1,5 +1,13 @@
 <?php
-
+/*
+if (file_exists($favicon = dirname($theme_object->filename) . '/favicon.ico')) {
+    $cache[$theme]['favicon'] = file_create_url($favicon);
+} else {
+    $cache[$theme]['favicon'] = file_create_url('misc/favicon.ico');
+} elseif ($cache[$theme]['favicon_path']) {
+    $cache[$theme]['favicon'] = file_create_url($cache[$theme]['favicon_path']);
+}
+*/
 function portfolio_preprocess_page(&$variables) {
     $variables['test_me'] = "HELLO WORLD (page.tpl)";
 
@@ -8,6 +16,7 @@ function portfolio_preprocess_page(&$variables) {
     if (!empty($variables['node']) && !empty($variables['node']->type)) {
       $variables['theme_hook_suggestions'][] = 'page__node__' . $variables['node']->type;
   }
+
 
 }
 function portfolio_preprocess_html(&$variables) {

e96b204b38292cbcac2362d54a7de73f5031b007
Seth Silesky <seth.silesky@gmail.com>
2016-04-20 11:47:53 -0500

    Revert "text displays in columns--add 'text-container' class to line 98. remove wrapping divs."
    This reverts commit 3e2588e738a097005ecd87e61a391e725157ca77.

--- sites/all/themes/portfolio/templates/node--content-microsite.tpl.php
+++ sites/all/themes/portfolio/templates/node--content-microsite.tpl.php
@@ -89,19 +89,27 @@
     print $bg_image_url_relative;
   }
   ?>') center / cover;">
-  <div class="section-content">
-      <?php print render($title_prefix); ?>
-      <?php if (!$page): ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <div class="text-container" ?>
-        <?php
-          hide($content['comments']);
-          hide($content['links']);
-          hide($content['field_background_image']);
-          print render($content);
-        ?>
-      </div>
+<div class="section-content">
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+
+
+  <div class="content"<?php print $content_attributes; ?>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      hide($content['field_background_image']);
+      print render($content);
+    ?>
   </div>
+</div>
+
+</div>
 </div>
